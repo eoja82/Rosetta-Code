6ed0480e4853f83a04715585d19b120b768fd0cf
Erik Oja <erik_oja@yahoo.com>
2020-08-08 20:11:06 -0500

    ABC Problem solution

--- rosettaCode.js
+++ rosettaCode.js
@@ -29,8 +29,47 @@ function getFinalOpenedDoors(numDoors) {
       result.push(i + 1);
     }
   });
-  console.log(result); // [ 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 ]
+  //console.log(result); // [ 1, 4, 9, 16, 25, 36, 49, 64, 81, 100 ]
   return result;
 }
 
 getFinalOpenedDoors(100);
+
+/* Rosetta Code: 24 game */
+
+/* Rosetta Code: 9 billion names of God the integer */
+
+/* Rosetta Code: ABC Problem
+You are given a collection of ABC blocks (e.g., childhood alphabet blocks). There are 20 blocks with two letters on each block. A complete alphabet is guaranteed amongst all sides of the blocks. The sample collection of blocks:
+(B O) (X K) (D Q) (C P) (N A) (G T) (R E) (T G) (Q D) (F S) (J W) (H U) (V I) (A N) (O B) (E R) (F S) (L Y) (P C) (Z M)
+Implement a function that takes a string (word) and determines whether the word can be spelled with the given collection of blocks.
+Some rules to keep in mind:
+    Once a letter on a block is used, that block cannot be used again.
+    The function should be case-insensitive. */
+
+function canMakeWord(word) {
+  const blocks = [["B", "O"], ["X", "K"], ["D", "Q"], ["C", "P"], ["N", "A"], ["G", "T"], ["R", "E"], ["T", "G"],
+                ["Q", "D"], ["F", "S"], ["J", "W"], ["H", "U"], ["V", "I"], ["A", "N"], ["O", "B"], ["E", "R"], 
+                ["F", "S"], ["L", "Y"], ["P", "C"], ["Z", "M"]]
+  let wordArr = word.toUpperCase().split(""),
+      matched = 0,
+      match = false
+  
+  for (let i = 0; i < wordArr.length; i++) {
+    for (let j = 0; j < blocks.length; j++) {
+      if (blocks[j][0] == wordArr[i] || blocks[j][1] == wordArr[i]) {
+        blocks.splice(j, 1)
+        matched++
+        break
+      }
+    } 
+    if (matched == wordArr.length) {
+      match = true
+      break
+    }
+  }
+  // console.log(match) // true
+  return match
+}
+
+canMakeWord("conFUSE")
